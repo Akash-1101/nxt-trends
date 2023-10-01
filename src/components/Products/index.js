17575e04f9c5d66aca460c6324322684516a4bce
Akash-1101 <akashgs1101@gmail.com>
2023-10-01 11:43:04 +0000

    sol

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        className="products-img"
+        alt="products"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      />
+    </div>
+  </>
+)
+export default Products
